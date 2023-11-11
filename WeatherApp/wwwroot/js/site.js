3670879958879ba7e8edeadd8919b20e32c7f086
Kenneth Tyler Somervile <tylersomerville55@gmail.com>
2023-11-11 15:29:56 -0500

    added jest tests
    added jest tests

--- WeatherApp/wwwroot/js/site.js
+++ WeatherApp/wwwroot/js/site.js
@@ -1,30 +1,58 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-
+window.$ = require('../lib/jquery/dist/jquery');
 // Write your JavaScript code.
-$("#btnGo").on("click", function () {
-    let longitude = $("#longitude").val();
-    let latitude = $("#latitude").val();
-    if (longitude == '' || latitude == '') {
-        $('#errorContainer').text("Please enter two values!");
-        $('#errorContainer').show();
-    }
-    else {
-        $.ajax({
-            url: "/Home/GetWeather",
-            data: {
-                longitude: longitude,
-                latitude: latitude
-            },
-            success: function (result) {
-                $("#weatherResult").html(result);
-            },
-            error: function () {
-                $("#weatherResult").text("Whoops something went wrong please check your inputs and try again!");
-            }
+$(document).ready(function () {
+    site.setUpElements();
+});
+
+const site = (() => {
+
+    const setUpElements = () => {
+        $("#btnGo").click(function () {
+            let longitude = $("#longitude").val();
+            let latitude = $("#latitude").val();
+            console.log("long,lat= " + longitude + ", " + latitude);
+            site.getWeather(longitude, latitude)
         });
+        $(".textInput").on("input", function () {
+            $('#errorContainer').hide();
+        });
+    };
+    const getWeather = (longitude, latitude) => {
+        console.log("called");
+        if (longitude == '' || latitude == '') {
+            $('#errorContainer').text("Please enter two values!");
+            $('#errorContainer').show();
+        }
+        else {
+            $.ajax({
+                url: "/Home/GetWeather",
+                data: {
+                    longitude: longitude,
+                    latitude: latitude
+                },
+                success: obj.handleSuccess,
+                error: obj.handleError
+            });
+        }
     }
-});
-$(".textInput").on("input", function () {
-    $('#errorContainer').hide();
-});
+    const handleError = () => {
+        $("#weatherResult").text("ERROR");
+    };
+    const handleSuccess = (result) => {
+        $("#weatherResult").html(result);
+    };
+    const render = () => {
+        return "<div>some stuff</div>";
+    };
+    const obj = {
+        setUpElements,
+        getWeather,
+        handleError,
+        handleSuccess,
+        render,
+    };
+    return obj;
+})();
+module.exports = site;
---
+++ WeatherApp/wwwroot/tests/site.test.js
@@ -0,0 +1,61 @@
+﻿
+const { handleSuccess } = require('../../wwwroot/js/site');
+const site = require('../../wwwroot/js/site');
+const $ = require('../lib/jquery/dist/jquery');
+describe("Ajax", () => {
+  
+
+    beforeEach(() => {
+        jest.restoreAllMocks();
+    });
+    it("Data should load", () => {
+        const ajaxSpy = jest.spyOn($, "ajax");
+        const functionType = ( site.handleError.toString())
+        site.getWeather(1, 3);
+        expect(ajaxSpy).toHaveBeenCalled();
+
+    });
+
+    it("should handle success", () => {
+        const logSpy = jest.spyOn(console, "log");
+        document.body.innerHTML =
+            '<div>' +
+            '  <div id="weatherResult"></div>' +
+            '  <input id="longitude" value="2"/>' +
+            '  <input id="latitude" value="1"/>' +
+            '  <button id="btnGo" />' +
+            '</div>';
+        site.handleSuccess("TEST")
+        expect($("#weatherResult").text()).toBe("TEST");
+    });
+    test("should handle error", () => {
+        const logSpy = jest.spyOn(console, "log");
+        document.body.innerHTML =
+            '<div>' +
+            '  <div id="weatherResult"></div>' +
+            '  <input id="longitude" value="2"/>' +
+            '  <input id="latitude" value="1"/>' +
+            '  <button id="btnGo" />' +
+            '</div>';
+        site.handleError();
+        let errorText = $("#weatherResult").text();
+        expect(errorText).toBe("ERROR");
+    });
+});
+test("button clicks", () => {
+    document.body.innerHTML =
+        '<div>' +
+    '  <div id="weahterResult"></div>' +
+    '  <input id="longitude" value="2"/>' +
+    '  <input id="latitude" value="1"/>' +
+        '  <button id="btnGo" />' +
+        '</div>';
+    
+
+    const spy = jest.spyOn(site, "getWeather");
+
+    let btn = document.getElementById("btnGo");
+    site.setUpElements();
+    btn.click();
+    expect(site.getWeather).toBeCalled();
+});
